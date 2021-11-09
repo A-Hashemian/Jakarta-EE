03e24e7f128a38abd9d572eda17646f52fdc4ccc
A-Hashemian <amin@hashemian.tech>
2021-11-09 23:57:14 +0300

    first

---
+++ first/src/main/java/Payara-Jakarta/com/App.java
@@ -0,0 +1,13 @@
+package Payara-Jakarta.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello jakarta ee" );
+    }
+}
